17a8ac1e295dfef1844e0bebf2a7bd463813d2b4
Tommi <19407724+Kabanossi@users.noreply.github.com>
2026-01-02 19:31:15 +0200

    Tehty deal_count2 ja top-lista

---
+++ TexasFuckEm/Classes/Top_Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TexasFuckEm.Classes
+{
+    public class Top_Player
+    {
+        public string Name { get; set; }
+        public int Money { get; set; }
+    }
+}
--- TexasFuckEm/Program.cs
+++ TexasFuckEm/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using TexasFuckEm.Classes;
 
 namespace TexasFuckEm
@@ -8,7 +10,23 @@ namespace TexasFuckEm
 
         static void Main()
         {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "scorelist.txt");
+            var full_list = File.ReadAllLines(filePath).ToList();
+
             int balance;
+            int inputY = Console.WindowHeight - 2;
+            int deal_count = 1;
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("TOP 5 PELAAJAT:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i + 1}. {full_list[i]}");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine();
 
             Console.Write("Anna pelaajan nimi:");
 
@@ -20,18 +38,14 @@ namespace TexasFuckEm
             int.TryParse(Console.ReadLine(), out balance);
 
 
-
             if (p != null)
             {
                 do
                 {
+                    var deck = new Deck();
 
-                    int deal_count = 1;
-                    //Console.CursorVisible = false;
 
                     Console.Clear();
-
-
 
                     Console.SetCursorPosition(0, 0);
                     Console.BackgroundColor = ConsoleColor.Red;
@@ -39,24 +53,33 @@ namespace TexasFuckEm
                     Console.WriteLine();
                     Console.BackgroundColor = ConsoleColor.Black;
 
-                    if (balance > 0)
+                    if (balance > 0 || deal_count == 2)
                     {
 
                         int voitto;
-                        var deck = new Deck();
 
-                        deck.Shuffle();
 
-                        //Ekat kortit
-                        balance--;
-                        p.Hand = deck.DealHand(5);  
-                        Console.WriteLine(p.ToString());
+                        if (deal_count == 1)
+                        {
+                            balance--;
 
-                        string komento = Console.ReadLine();
+                            deck.Shuffle();
+
+                            //Ekat kortit
+
+                            p.Hand = deck.DealHand(5);
+                            Console.WriteLine(p.ToString());
+
+                        }
+
+
 
 
                         if (deal_count == 2)
                         {
+                            //tokat kortit
+
+                            Console.WriteLine(p.ToString());
                             p.EvaluateHand();
                             voitto = Profit(p.CurrentHandType);
 
@@ -71,28 +94,75 @@ namespace TexasFuckEm
                             else
                             {
                                 Console.WriteLine("Ei voittoa.");
-                            } 
+                            }
                         }
 
                     }
                     else
                     {
-                        Console.WriteLine("EI RAHAA!");
+                        Console.WriteLine("EI RAHAA! PELI LOPPUI!");
+                        break;
                     }
 
                     DrawStatus(p.Name, balance);
-
-
-
-                    // input aina rivillä ennen statusriviä
-                    int inputY = Console.WindowHeight - 2;
                     Console.SetCursorPosition(0, inputY);
-                    Console.Write("Komento: ");
 
-                    string command = ReadInputAt(9, inputY);
+                    string d1 = "Valitse poistettavat kortit: ";
+                    string d2 = "Paina Enter pelataksesi seuraavan käden.";
+
+                    int c1 = d1.Length;
+                    int c2 = d2.Length;
+
+                    Console.Write(deal_count == 1 ? d1 : d2);
+
+
+                    // input aina rivillä ennen alariviä
+                    string command = ReadInputAt(deal_count == 1 ? c1 : c2, inputY);
+
 
                     if (command == "x")
+                    {
+                        Top_Player tp = new Top_Player();
+                        tp.Name = p.Name;
+                        tp.Money = balance;
+
+                        int ranking = AddToScorelist(tp, full_list, filePath);
+
+                        Console.Clear();
+                        Console.SetCursorPosition(0, 0);
+                        Console.WriteLine($"Kiitos pelaamisesta! Sinulle jäi {balance} euroa. Sijoituit sijalle {ranking}!");
                         break;
+                    }
+
+
+
+                    int[] discard;
+
+                    if (deal_count == 1)
+                    {
+                        try
+                        {
+                            discard = Array.ConvertAll(command.Split(','), int.Parse);
+
+                            foreach (var i in discard.OrderByDescending(x => x))
+                            {
+                                p.Hand.RemoveAt(i - 1);
+                            }
+
+                            p.Hand.AddRange(deck.DealHand(discard.Length));
+
+                            deal_count = 2;
+                        }
+                        catch (Exception e)
+                        {
+                            deal_count = 2;
+                        }
+                    }
+                    else
+                    {
+                        deal_count = 1;
+                    }
+
 
                 } while (true);
             }
@@ -109,7 +179,6 @@ namespace TexasFuckEm
             Console.SetCursorPosition(0, y);
             Console.Write($"|{player}|Rahaa: {balance}|");
         }
-
         private static int Profit(string type)
         {
             switch (type)
@@ -130,8 +199,7 @@ namespace TexasFuckEm
                     return 0;
             }
         }
-
-        static string ReadInputAt(int x, int y)
+        private static string ReadInputAt(int x, int y)
         {
             Console.SetCursorPosition(x, y);
             Console.CursorVisible = true;
@@ -162,6 +230,41 @@ namespace TexasFuckEm
             Console.CursorVisible = false;
             return input;
         }
+        private static int AddToScorelist(Top_Player player, List<string> full_list, string filepath)
+        {
+            List<Top_Player> top_Players = new List<Top_Player>();
+
+            top_Players.Add(player);
+
+            foreach (var x in full_list)
+            {
+                var match = Regex.Match(x, @"^(.*),\s*(\d+)\s*euroa$");
+
+                string n = match.Groups[1].Value;
+                int m = int.Parse(match.Groups[2].Value);
+
+                top_Players.Add(new Top_Player { Money = m, Name = n });
+            }
+
+            top_Players = top_Players.OrderByDescending(x => x.Money).ToList();
+
+            int standing = top_Players.IndexOf(player) + 1;
+
+            var lines = new List<string>();
+
+            for (int i = 0; i < top_Players.Count; i++)
+            {
+                lines.Add($"{top_Players[i].Name}, {top_Players[i].Money} euroa");
+            }
+
+            if (File.Exists(filepath))
+            {
+
+                File.WriteAllLines(filepath, lines);
+            }
+            return standing;
+        }
+
 
 
     }
