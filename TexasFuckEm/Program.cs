406fe895fadc20ef3b657f7a540f3570de562174
Tommi <19407724+Kabanossi@users.noreply.github.com>
2026-01-03 20:15:01 +0200

    Panosta ja muuttujia. Korjauksia vaatii

--- TexasFuckEm/Program.cs
+++ TexasFuckEm/Program.cs
@@ -9,74 +9,33 @@ namespace TexasFuckEm
     class Program
     {
 
+        static Player p;
+        static string filePath;
+        static List<string> full_list;
+        static int bet;
+        static int inputY;
+        static Deck deck;
         static void Main()
         {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "scorelist.txt");
-            var full_list = File.ReadAllLines(filePath).ToList();
+            filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "scorelist.txt");
+            full_list = File.ReadAllLines(filePath).ToList();
+            inputY = Console.WindowHeight - 2;
+            int balance = 50;
 
-            int balance;
-            int inputY = Console.WindowHeight - 2;
+            //alkuruutu
+            DrawStartUp();
+            
             int deal_count = 1;
-
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine("TOP 10 PELAAJAT:");
-
-            for (int i = 0; i < full_list.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}. {full_list[i]}");
-            }
-
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine();
-
-            Console.Write("Anna pelaajan nimi:");
-
-
-            var p = new Player() { Name = Console.ReadLine() ?? "Idiootti" };
-
-            Console.Write("Anna rahamäärä:");
-
-            int.TryParse(Console.ReadLine(), out balance);
-
 
             if (p != null)
             {
                 do
                 {
-                    var deck = new Deck();
+                    deck = new Deck();
 
-
+                    //yläbanner ja voittotaulu
                     Console.Clear();
-
-                    //Yläpaneeli
-                    Console.SetCursorPosition(0, 0);
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Syöttämällä \"x\", peli loppuu");
-                    Console.WriteLine();
-                    Console.BackgroundColor = ConsoleColor.Black;
-
-                    //Voittotaulu
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 0);
-                    Console.WriteLine("Värisuora.....500");
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 1);
-                    Console.WriteLine("Neloset.......100");
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 2);
-                    Console.WriteLine("Täyskäsi.......50");
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 3);
-                    Console.WriteLine("Väri...........25");
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 4);
-                    Console.WriteLine("Suora..........15");
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 5);
-                    Console.WriteLine("Kolmoset.......10");
-                    Console.SetCursorPosition(Console.WindowWidth - 17, 6);
-                    Console.WriteLine("Kaksi paria.....2");
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    Console.SetCursorPosition(0, 2);
-
+                    DrawBannerAndProfitList();
 
 
                     if (balance > 0 || deal_count == 2)
@@ -88,7 +47,7 @@ namespace TexasFuckEm
                         if (deal_count == 1)
                         {
 
-                            balance--;
+                            balance -= bet;
 
                             deck.Shuffle();
 
@@ -97,9 +56,6 @@ namespace TexasFuckEm
                             Console.WriteLine(p.ToString());
 
                         }
-
-
-
 
                         if (deal_count == 2)
                         {
@@ -130,35 +86,20 @@ namespace TexasFuckEm
                         break;
                     }
 
-                    DrawStatus(p.Name, balance);
+                    DrawStatus();
                     Console.SetCursorPosition(0, inputY);
-
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, inputY);
                     string d1 = "Valitse poistettavat kortit: ";
-                    string d2 = "Paina Enter pelataksesi seuraavan käden.";
+                    string d2 = "Enter: Seuraava käsi k 1 - 5 : Vaihda panosta: ";
 
                     int c1 = d1.Length;
                     int c2 = d2.Length;
 
                     Console.Write(deal_count == 1 ? d1 : d2);
 
-
                     // input aina rivillä ennen alariviä
                     string command = ReadInputAt(deal_count == 1 ? c1 : c2, inputY);
-
-
-                    if (command == "x")
-                    {
-                        Top_Player tp = new Top_Player();
-                        tp.Name = p.Name;
-                        tp.Money = balance;
-
-                        int ranking = AddToScorelist(tp, full_list, filePath);
-
-                        Console.Clear();
-                        Console.SetCursorPosition(0, 0);
-                        Console.WriteLine($"Kiitos pelaamisesta! Sinulle jäi {balance} euroa. {(ranking > 0 ? $"Sijoituit sijalle {ranking}!" : "Et päässyt Top 10:n.")}");
-                        break;
-                    }
 
 
 
@@ -189,6 +130,9 @@ namespace TexasFuckEm
                         deal_count = 1;
                     }
 
+                    DetermineCommand(command, deal_count);
+
+                    if (command == "x") break;
 
                 } while (true);
             }
@@ -197,38 +141,38 @@ namespace TexasFuckEm
 
         }
 
-        private static void DrawStatus(string player, int balance)
+        private static void DrawStatus()
         {
             int y = Console.WindowHeight - 1;
             Console.SetCursorPosition(0, y);
             Console.Write(new string(' ', Console.WindowWidth - 1));
             Console.SetCursorPosition(0, y);
-            Console.Write($"|{player}|Rahaa: {balance}|");
+            Console.Write($"|{p.Name}|Rahaa: {balance}|");
         }
         private static int Profit(string type)
         {
             switch (type)
             {
                 case "Värisuora":
-                    return 500;
+                    return bet * 500;
 
                 case "Neloset":
-                    return 100;
+                    return bet * 100;
 
                 case "Täyskäsi":
-                    return 50;
+                    return bet * 50;
 
                 case "Väri":
-                    return 25;
+                    return bet * 25;
 
                 case "Suora":
-                    return 15;
+                    return bet * 15;
 
                 case "Kolmoset":
-                    return 10;
+                    return bet * 10;
 
                 case "Kaksi paria":
-                    return 2;
+                    return bet * 2;
 
                 default:
                     return 0;
@@ -306,6 +250,103 @@ namespace TexasFuckEm
                 return standing;
             }
 
+        }
+        private static void DrawBannerAndProfitList()
+        {
+            //Yläpaneeli
+            Console.SetCursorPosition(0, 0);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Syöttämällä \"x\" missä tahansa vaiheessa, peli loppuu");
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            //Voittotaulu
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(Console.WindowWidth - 17, 0);
+            Console.WriteLine("Värisuora.....500");
+            Console.SetCursorPosition(Console.WindowWidth - 17, 1);
+            Console.WriteLine("Neloset.......100");
+            Console.SetCursorPosition(Console.WindowWidth - 17, 2);
+            Console.WriteLine("Täyskäsi.......50");
+            Console.SetCursorPosition(Console.WindowWidth - 17, 3);
+            Console.WriteLine("Väri...........25");
+            Console.SetCursorPosition(Console.WindowWidth - 17, 4);
+            Console.WriteLine("Suora..........15");
+            Console.SetCursorPosition(Console.WindowWidth - 17, 5);
+            Console.WriteLine("Kolmoset.......10");
+            Console.SetCursorPosition(Console.WindowWidth - 17, 6);
+            Console.WriteLine("Kaksi paria.....2");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(Console.WindowWidth - 17, 7);
+            Console.WriteLine($"PANOS: {bet}");
+
+            Console.SetCursorPosition(0, 2);
+        }
+        private static void DetermineCommand(string command, int dc)
+        {
+            switch (command)
+            {
+                case "x":
+                    //lopetus
+                    Top_Player tp = new Top_Player();
+                    tp.Name = p.Name;
+                    tp.Money = balance;
+
+                    int ranking = AddToScorelist(tp, full_list, filePath);
+
+                    Console.Clear();
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine($"Kiitos pelaamisesta! Sinulle jäi {balance} euroa. {(ranking > 0 ? $"Sijoituit sijalle {ranking}!" : "Et päässyt Top 10:n.")}");
+                    break;
+
+                case "1":case "2":case "3": case "4":case "5":
+                    //panoksen vaihto
+                    int possibleBet;
+                    bool isnumb = int.TryParse(command, out possibleBet);
+
+                    if (isnumb && dc == 1)
+                    {
+                        bet = possibleBet;
+                        bet = bet > 5 ? 5 : bet;
+                        //deal_count = 1;
+                    }
+                    break;
+
+                default:
+                    //korttien vaihto
+
+                    break;
+            }
+        }
+        private static void DrawStartUp()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("TOP 10 PELAAJAT:");
+
+            for (int i = 0; i < full_list.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {full_list[i]}");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+
+            Console.Write("Anna pelaajan nimi: ");
+
+
+            p = new Player() { Name = Console.ReadLine() ?? "Idiootti" };
+            p.Name = p.Name == "" ? "Idiootti" : p.Name;
+
+            Console.WriteLine($"Rahamäärä: {balance}");
+
+            //int.TryParse(Console.ReadLine(), out balance);
+            
+
+            Console.Write("Anna panos (1 - 5) ja aloita: ");
+            bet = int.TryParse(Console.ReadLine(), out bet) ? bet : 1;
+            bet = bet > 5 ? 5 : bet;
         }
 
 
